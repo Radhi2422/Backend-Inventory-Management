a107f33b288c621afac678e30d5bea4f2e4820f1
Radhika Mittal <106824237+Radhi2422@users.noreply.github.com>
2026-07-16 17:46:42 +0530

    Add files via upload

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -7,13 +7,11 @@ module.exports=async(req,res,next)=>{
         const token=req.headers.authorization?.split(" ")[1];
 
         if(!token){
-
             return res.status(401).json({
                 message:"Token Missing"
             });
 
         }
-console.log("Auth failing");
 
         const decoded=jwt.verify(token,process.env.JWT_SECRET);
 
